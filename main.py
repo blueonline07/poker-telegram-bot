5b12eef4bd4e0d40b8012afc537d95098192c4c0
ldkhang <ldkhang1201@gmail.com>
2025-01-23 23:08:41 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import os
+from user import UserTable
+
+import telebot
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN') or '7328513195:AAHH_dMmGYcCmCFp-FdnGDcV_SrVDTIvHZc'
+bot = telebot.TeleBot(BOT_TOKEN)
+tb = UserTable()
+
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, "Hello, {} {}!".format(message.from_user.first_name, message.from_user.last_name))
+
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    pass
+
+
+@bot.message_handler(commands=['join'])
+def handle_join(message):
+    try:
+        tb.add_user(message.from_user.id, message.from_user.first_name, message.from_user.last_name)
+        bot.send_message(message.chat.id, "Congratulations, {}! You have joined the table".format(message.from_user.username))
+    except Exception as e:
+        print(e)
+        bot.send_message(message.chat.id, "Something went wrong. Please try again later")
+
+@bot.message_handler(commands=['buy','sell'])
+def handle_transaction(message):
+    try:
+        op = message.text.split(' ')[0][1:]
+        amount = int(message.text.split(' ')[1])
+        print(op)
+        if op == 'buy':
+            tb.update_balance(message.from_user.id, -amount)
+        elif op == 'sell':
+            tb.update_balance(message.from_user.id, amount)
+
+        users = tb.get_all_users()
+        user_ids = [user["id"] for user in users]
+        for user_id in user_ids:
+            bot.send_message(user_id, f"{message.from_user.first_name} {message.from_user.last_name} {op}s {amount}. Their balance is now {tb.get_user(message.from_user.id)["balance"]}.")
+
+    except Exception as e:
+        print(e)
+        bot.send_message(message.chat.id, "Something went wrong. Please try again later")
+
+
+@bot.message_handler(commands=['check'])
+def check_balance(message):
+    bot.send_message(message.chat.id, tb.get_user(message.from_user.id)["balance"])
+
+
+@bot.message_handler(commands=['all'])
+def show_table(message):
+    users = tb.get_all_users()
+    for user in users:
+        bot.send_message(message.chat.id, f"{user['first_name']} {user['last_name']}: {user['balance']}")
+
+bot.infinity_polling()
---
+++ user.py
@@ -0,0 +1,57 @@
+import sqlite3
+
+class UserTable:
+    def __init__(self, db_path='data.db'):
+        self.db_path = db_path
+
+    def get_connection(self):
+        return sqlite3.connect(self.db_path)
+
+    def add_user(self, id, first_name, last_name, balance=0):
+        with self.get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute('''
+            INSERT OR IGNORE INTO users (id, first_name, last_name, balance)
+            VALUES (?, ?, ?, ?)
+            ''', (id, first_name, last_name, balance))
+            conn.commit()
+
+    def update_balance(self, id, amount):
+        with self.get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute('''
+            UPDATE users
+            SET balance = balance + ?
+            WHERE id = ?
+            ''', (amount, id))
+            conn.commit()
+
+    def get_user(self, id):
+        with self.get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute('SELECT * FROM users WHERE id = ?', (id,))
+            row = cursor.fetchone()
+            if row:
+                return {
+                    'id': row[0],
+                    'first_name': row[1],
+                    'last_name': row[2],
+                    'balance': row[3]
+                }
+            return None
+
+
+    def get_all_users(self):
+        with self.get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute('SELECT * FROM users')
+            rows = cursor.fetchall()
+            ll = []
+            for row in rows:
+                ll.append({
+                    'id': row[0],
+                    'first_name': row[1],
+                    'last_name': row[2],
+                    'balance': row[3]
+                })
+            return ll
